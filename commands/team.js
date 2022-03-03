725984fa11ff1ec0240b94e89dc9d298e576fecb
Ben <ben.ng@outlook.com>
2022-03-03 01:23:44 -0500

    added teams skeleton with more detail

--- commands/team.js
+++ commands/team.js
@@ -3,17 +3,24 @@ module.exports = {
     description: 'Adds a player to a team, or creates it if there isn\'t a matching team already',
     usage: '<team-name>',
     execute(message, args) {
-        console.log("args", args);
-        //if team exists
-        //add author to permission
+        if(args.length !== 1) return;
+
+        if(!doesTeamExist(args[0], message.guild.roles.cache)) {
+            //create category
+            //create text
+            //create voice
+        }
         
-        //else
-        //create category
-        //create text
-        //create voice
         
         //add author to role
 
         //send welcome message
     }
+}
+
+doesTeamExist = (teamName, roles) => {
+    const filteredRoles = roles.filter(role => role.name == teamName);
+    let filteredArray = Array.from(filteredRoles);
+    filteredArray = [...filteredArray];
+    return filteredArray.length > 0;
 }
